243b4fe5c03f074aef4e8d377db9ed82ea8c602f
vspaz <vladimir.edelman@gmail.com>
2021-12-01 19:47:17 +0100

    add Java class that deals with python

---
+++ src/org/vspaz/Main.java
@@ -0,0 +1,29 @@
+package org.vspaz;
+
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Value;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static double ComputeTotalWithPythonAndNumpy() {
+        Context ctx = Context.newBuilder().allowAllAccess(true).build();
+        File pythonModule = new File("/home/vspaz/graal_tests/pure_python.py");
+        try {
+            ctx.eval(Source.newBuilder("python", pythonModule).build());
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        Value computeTotal = ctx.getBindings("python").getMember("compute_total");
+        Object[] nums = {1.0, 2.23, 3.49494, 4.40404, 5.10110, 181.101, 133.11};
+        Value computedResult = computeTotal.execute(nums);
+        return computedResult.asDouble();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ComputeTotalWithPythonAndNumpy()); // -> 330.44108
+    }
+}
